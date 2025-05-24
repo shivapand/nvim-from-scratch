a5948904f4127a5d8785d87598d8c2fb95875b0b
shivapand <shiva.pandit@ecolibrium.io>
2025-05-25 00:06:29 +0530

    done for tonight

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,6 +1,11 @@
-
-vim.api.nvim_create_autocmd("BufNew", {
+vim.api.nvim_create_autocmd("TabNew", {
   callback = function()
     vim.cmd("cd `pwd`")
+  end
+})
+
+vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+  callback = function()
+    print("HERE>>>>>>>")
   end
 })
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -39,3 +39,33 @@ vim.keymap.set("v", "<leader>a", function()
   })
 end, { desc = "Format current file" })
 
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+  callback = function(event)
+    local map = function(keys, func, desc, mode)
+      mode = mode or 'n'
+      vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+    end
+
+    map('gn', vim.lsp.buf.rename, '[R]e[n]ame')
+
+    map('ga', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
+
+    map('gr', require('fzf-lua').lsp_references, '[G]oto [R]eferences')
+
+    map('gi', require('fzf-lua').lsp_implementations, '[G]oto [I]mplementation')
+
+    map('gd', require('fzf-lua').lsp_definitions, '[G]oto [D]efinition')
+
+    map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+
+    map('gO', require('fzf-lua').lsp_document_symbols, 'Open Document Symbols')
+
+    map('gW', require('fzf-lua').lsp_live_workspace_symbols, 'Open Workspace Symbols')
+
+    map('gt', require('fzf-lua').lsp_typedefs, '[G]oto [T]ype Definition')
+
+    map('gk', vim.diagnostic.open_float, 'Open Diagnostics in Float')
+  end,
+})
